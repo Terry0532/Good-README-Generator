b591fe2d8d7e2241dcf19a351347575f8c77f0b8
Terry <t3rrygong@gmail.com>
2020-04-25 15:33:37 -0600

    added questions

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-const questions = [
+const inquirer = require("inquirer");
 
+const questions = [
+    {
+        type: "input",
+        message: "Your project title?",
+        name: "projectName"
+    }, {
+        type: "input",
+        message: "A short description of your project.",
+        name: "projectDescription"
+    }, {
+        type: "input",
+        message: "How to install it?",
+        name: "projectInstallation"
+    }, {
+        type: "input",
+        message: "What open source packages used for this project?",
+        name: "credit"
+    }, {
+        type: "input",
+        message: "What are the key features?",
+        name: "features"
+    }, {
+        type: "input",
+        message: "GitHub profile link.",
+        name: "githubLink"
+    }, {
+        type: "list",
+        message: "Chose your license.",
+        name: "license",
+        choices: ["GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "MIT License", "Boost Software License 1.0", "The Unlicense"]
+    }
 ];
 
 function writeToFile(fileName, data) {
 }
 
 function init() {
-
+    inquirer
+        .prompt(questions)
+        .then(response => console.log(response));
 }
 
 init();
